f341cc8505c85dd5c6f3e0d856c58962a99c12ed
Berna Kiziler <bkiziler@k1m33s04.42istanbul.com.tr>
2023-02-25 20:24:30 +0300

    last

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: bkiziler <bkiziler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 15:53:40 by bkiziler          #+#    #+#             */
-/*   Updated: 2023/02/25 18:48:53 by bkiziler         ###   ########.fr       */
+/*   Updated: 2023/02/25 20:19:29 by bkiziler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,9 @@ void	slidebits(char c)
 	while (i <= 7)
 	{
 		if (c & 1)
-		{
 			kill(spid, SIGUSR1);
-			write(1, "1", 1);
-		}
 		else
-		{
 			kill(spid, SIGUSR2);
-			write(1,"0", 1);
-		}
 		c = c >> 1;
 		i++;
 		usleep(100);
